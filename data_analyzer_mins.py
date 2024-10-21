ceeacc323d412aeed65d71e007995c8974ca3840
Karan96Kaushik <karankshk96@gmail.com>
2024-10-21 10:59:37 +0100

    15 min analyser fix

--- data_analyzer_mins.py
+++ data_analyzer_mins.py
@@ -4,31 +4,24 @@ from wh_urls import webhook_url, webhook_error_url, webhook_details_url
 
 def analyse_data(df, sym, tolerance=0.01):
     try:
-        # if (sym != 'HDFCLIFE'): 
-        #     return None
         df['44_day_ma'] = df['close'].rolling(window=44).mean()
         df['7_vol_ma'] = df['volume'].rolling(window=7).mean()
         events = []
         i = len(df) - 1
 
-        if pd.notna(df.iloc[i]['44_day_ma']):
-            print(df.iloc[i-1]['44_day_ma'] < df.iloc[i]['44_day_ma'] ,
-                df.iloc[i-2]['44_day_ma'] < df.iloc[i-1]['44_day_ma'] ,
-                df.iloc[i-3]['44_day_ma'] < df.iloc[i-2]['44_day_ma'] ,
-                df.iloc[i-4]['44_day_ma'] < df.iloc[i-3]['44_day_ma'] ,
-                (abs(df.iloc[i]['44_day_ma'] - df.iloc[i]['low']) ,'<', (df.iloc[i]['44_day_ma']*tolerance) ,
-                 (df.iloc[i]['44_day_ma'] > df.iloc[i]['low'] ,'and' , df.iloc[i]['44_day_ma'] ,'<', df.iloc[i]['high'])), 'and' ,
-                (df.iloc[i-1]['close'] ,'>', df.iloc[i-1]['open'] ,'or',
-                 (df.iloc[i-1]['high'] - df.iloc[i-1]['close']) ,'<', (df.iloc[i-1]['close'] ,'-', df.iloc[i-1]['low'])))
+        print(df)
+        print(df.iloc[i-1])
 
-            if (df.iloc[i-1]['44_day_ma'] < df.iloc[i]['44_day_ma'] and     # 44 day ma is going up
+        if pd.notna(df.iloc[i]['44_day_ma']):
+            if (
                 df.iloc[i-2]['44_day_ma'] < df.iloc[i-1]['44_day_ma'] and   # 44 day ma is going up
                 df.iloc[i-3]['44_day_ma'] < df.iloc[i-2]['44_day_ma'] and   # 44 day ma is going up
                 df.iloc[i-4]['44_day_ma'] < df.iloc[i-3]['44_day_ma'] and   # 44 day ma is going up
-                (abs(df.iloc[i]['44_day_ma'] - df.iloc[i]['low']) < (df.iloc[i]['44_day_ma']*tolerance) or   # price is near 44 day ma
-                 (df.iloc[i]['44_day_ma'] > df.iloc[i]['low'] and df.iloc[i]['44_day_ma'] < df.iloc[i]['high'])) and   # price is between low and high
-                (df.iloc[i]['close'] > df.iloc[i-1]['close'] or   # closing price is up
-                 (df.iloc[i]['high'] - df.iloc[i]['open']) < (df.iloc[i]['open'] - df.iloc[i]['low']))):   # close is near high than low
+                df.iloc[i-5]['44_day_ma'] < df.iloc[i-4]['44_day_ma'] and   # 44 day ma is going up
+                (abs(df.iloc[i-1]['44_day_ma'] - df.iloc[i-1]['low']) < (df.iloc[i-1]['44_day_ma']*tolerance) or   # price is near 44 day ma
+                 (df.iloc[i-1]['44_day_ma'] > df.iloc[i-1]['low'] and df.iloc[i-1]['44_day_ma'] < df.iloc[i-1]['high'])) and   # price is between low and high
+                (df.iloc[i-1]['close'] > df.iloc[i-1]['open'] or   # closing price is up
+                 (df.iloc[i-1]['high'] - df.iloc[i-1]['close']) < (df.iloc[i-1]['close'] - df.iloc[i-1]['low']))):   # close is near high than low
                 
                 events.append(df.iloc[i-2])
                 events.append(df.iloc[i-1])
@@ -44,8 +37,6 @@ def analyse_data(df, sym, tolerance=0.01):
 
 def analyse_data_downward(df, sym, tolerance=0.01):
     try:
-        # if (sym != 'HDFCLIFE'): 
-        #     return None
 
         df['44_day_ma'] = df['close'].rolling(window=44).mean()
         df['7_vol_ma'] = df['volume'].rolling(window=7).mean()
@@ -53,14 +44,15 @@ def analyse_data_downward(df, sym, tolerance=0.01):
         i = len(df) - 1
 
         if pd.notna(df.iloc[i]['44_day_ma']):
-            if (df.iloc[i-1]['44_day_ma'] > df.iloc[i]['44_day_ma'] and   # 44 day ma is going down
+            if (
                 df.iloc[i-2]['44_day_ma'] > df.iloc[i-1]['44_day_ma'] and   # 44 day ma is going down
                 df.iloc[i-3]['44_day_ma'] > df.iloc[i-2]['44_day_ma'] and   # 44 day ma is going down
                 df.iloc[i-4]['44_day_ma'] > df.iloc[i-3]['44_day_ma'] and   # 44 day ma is going down
-                (abs(df.iloc[i]['44_day_ma'] - df.iloc[i]['high']) < (df.iloc[i]['44_day_ma']*tolerance) or   # price is near 44 day ma
-                 (df.iloc[i]['44_day_ma'] > df.iloc[i]['low'] and df.iloc[i]['44_day_ma'] < df.iloc[i]['high'])) and   # price is between low and high
-                (df.iloc[i]['close'] < df.iloc[i-1]['close'] or   # closing price is down
-                 (df.iloc[i]['high'] - df.iloc[i]['open']) > (df.iloc[i]['open'] - df.iloc[i]['low']))):   # close is near high than low
+                df.iloc[i-5]['44_day_ma'] > df.iloc[i-4]['44_day_ma'] and   # 44 day ma is going up
+                (abs(df.iloc[i-1]['44_day_ma'] - df.iloc[i-1]['high']) < (df.iloc[i-1]['44_day_ma']*tolerance) or   # price is near 44 day ma
+                 (df.iloc[i-1]['44_day_ma'] > df.iloc[i-1]['low'] and df.iloc[i-1]['44_day_ma'] < df.iloc[i-1]['high'])) and   # price is between low and high
+                (df.iloc[i-1]['close'] < df.iloc[i-1]['open'] or   # closing price is down
+                 (df.iloc[i-1]['high'] - df.iloc[i-1]['close']) > (df.iloc[i-1]['close'] - df.iloc[i-1]['low']))):   # close is near high than low
                 
                 events.append(df.iloc[i-2])
                 events.append(df.iloc[i-1])
